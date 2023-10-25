d957dd7a1e6f87bb6bbc6ec97ac34320fc4f4ee0
JustALoOp <kubabuko123@gmail.com>
2023-10-25 23:21:26 +0200

    Zmiany PictureBox

--- Galaktyka Matematyki/Form1.Designer.cs
+++ Galaktyka Matematyki/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Galaktyka_Matematyki
+﻿using System.Drawing.Drawing2D;
+
+namespace Galaktyka_Matematyki
 {
     partial class Form1
     {
@@ -28,12 +30,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBoxWithInterpolationMode1 = new PictureBoxWithInterpolationMode();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxWithInterpolationMode1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxWithInterpolationMode1
+            // 
+            pictureBoxWithInterpolationMode1.BackColor = Color.Transparent;
+            pictureBoxWithInterpolationMode1.Image = (Image)resources.GetObject("pictureBoxWithInterpolationMode1.Image");
+            pictureBoxWithInterpolationMode1.InterpolationMode = InterpolationMode.NearestNeighbor;
+            pictureBoxWithInterpolationMode1.Location = new Point(55, 116);
+            pictureBoxWithInterpolationMode1.Name = "pictureBoxWithInterpolationMode1";
+            pictureBoxWithInterpolationMode1.Size = new Size(314, 128);
+            pictureBoxWithInterpolationMode1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxWithInterpolationMode1.TabIndex = 0;
+            pictureBoxWithInterpolationMode1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1264, 985);
+            Controls.Add(pictureBoxWithInterpolationMode1);
+            DoubleBuffered = true;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBoxWithInterpolationMode1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBoxWithInterpolationMode pictureBoxWithInterpolationMode1;
     }
 }
