a6deba630b8870ba4431f1598cd05daef0b02824
BukowskiJakub <s188826@student.pg.edu.pl>
2023-12-22 09:16:41 +0100

    update

--- Galaktyka Matematyki/Form1.Designer.cs
+++ Galaktyka Matematyki/Form1.Designer.cs
@@ -32,6 +32,15 @@ namespace Galaktyka_Matematyki
         {
             label1 = new Label();
             pointsDisplay = new Label();
+            debugLabel = new Label();
+            mathMenu = new Panel();
+            result_box = new TextBox();
+            equalSign_label = new Label();
+            sendButton = new Button();
+            signLabel = new Label();
+            greenCount_box = new TextBox();
+            redCount_box = new TextBox();
+            mathMenu.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -40,9 +49,9 @@ namespace Galaktyka_Matematyki
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Consolas", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(14, 12);
+            label1.Location = new Point(12, 9);
             label1.Name = "label1";
-            label1.Size = new Size(139, 43);
+            label1.Size = new Size(111, 34);
             label1.TabIndex = 2;
             label1.Text = "Punkty";
             label1.TextAlign = ContentAlignment.MiddleCenter;
@@ -53,28 +62,125 @@ namespace Galaktyka_Matematyki
             pointsDisplay.BackColor = Color.Transparent;
             pointsDisplay.Font = new Font("Consolas", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             pointsDisplay.ForeColor = Color.White;
-            pointsDisplay.Location = new Point(147, 12);
+            pointsDisplay.Location = new Point(129, 9);
             pointsDisplay.Name = "pointsDisplay";
-            pointsDisplay.Size = new Size(179, 43);
+            pointsDisplay.Size = new Size(143, 34);
             pointsDisplay.TabIndex = 3;
             pointsDisplay.Text = "{points}";
             pointsDisplay.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // debugLabel
+            // 
+            debugLabel.AutoSize = true;
+            debugLabel.BackColor = Color.Transparent;
+            debugLabel.Font = new Font("Consolas", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            debugLabel.ForeColor = Color.White;
+            debugLabel.Location = new Point(12, 97);
+            debugLabel.Name = "debugLabel";
+            debugLabel.Size = new Size(127, 34);
+            debugLabel.TabIndex = 4;
+            debugLabel.Text = "numbers";
+            debugLabel.TextAlign = ContentAlignment.MiddleCenter;
+            debugLabel.Visible = false;
+            // 
+            // mathMenu
+            // 
+            mathMenu.BackColor = Color.Gray;
+            mathMenu.Controls.Add(result_box);
+            mathMenu.Controls.Add(equalSign_label);
+            mathMenu.Controls.Add(sendButton);
+            mathMenu.Controls.Add(signLabel);
+            mathMenu.Controls.Add(greenCount_box);
+            mathMenu.Controls.Add(redCount_box);
+            mathMenu.Location = new Point(365, 301);
+            mathMenu.Name = "mathMenu";
+            mathMenu.Size = new Size(614, 301);
+            mathMenu.TabIndex = 5;
+            mathMenu.Visible = false;
+            // 
+            // result_box
+            // 
+            result_box.BackColor = Color.FromArgb(224, 224, 224);
+            result_box.BorderStyle = BorderStyle.None;
+            result_box.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            result_box.Location = new Point(453, 71);
+            result_box.Name = "result_box";
+            result_box.Size = new Size(100, 75);
+            result_box.TabIndex = 5;
+            result_box.TextAlign = HorizontalAlignment.Center;
+            // 
+            // equalSign_label
+            // 
+            equalSign_label.AutoSize = true;
+            equalSign_label.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            equalSign_label.Location = new Point(380, 71);
+            equalSign_label.Name = "equalSign_label";
+            equalSign_label.Size = new Size(67, 75);
+            equalSign_label.TabIndex = 4;
+            equalSign_label.Text = "=";
+            // 
+            // sendButton
+            // 
+            sendButton.AutoSize = true;
+            sendButton.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            sendButton.Location = new Point(241, 230);
+            sendButton.Name = "sendButton";
+            sendButton.Size = new Size(143, 40);
+            sendButton.TabIndex = 3;
+            sendButton.Text = "Zatwierdź";
+            sendButton.UseVisualStyleBackColor = true;
+            sendButton.Click += sendButton_Click;
+            // 
+            // signLabel
+            // 
+            signLabel.AutoSize = true;
+            signLabel.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            signLabel.Location = new Point(185, 71);
+            signLabel.Name = "signLabel";
+            signLabel.Size = new Size(67, 75);
+            signLabel.TabIndex = 2;
+            signLabel.Text = "?";
+            // 
+            // greenCount_box
+            // 
+            greenCount_box.BackColor = Color.LightGreen;
+            greenCount_box.BorderStyle = BorderStyle.None;
+            greenCount_box.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            greenCount_box.Location = new Point(258, 71);
+            greenCount_box.Name = "greenCount_box";
+            greenCount_box.Size = new Size(100, 75);
+            greenCount_box.TabIndex = 1;
+            greenCount_box.TextAlign = HorizontalAlignment.Center;
+            // 
+            // redCount_box
+            // 
+            redCount_box.BackColor = Color.LightCoral;
+            redCount_box.BorderStyle = BorderStyle.None;
+            redCount_box.Font = new Font("Consolas", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            redCount_box.Location = new Point(64, 71);
+            redCount_box.Name = "redCount_box";
+            redCount_box.Size = new Size(100, 75);
+            redCount_box.TabIndex = 0;
+            redCount_box.TextAlign = HorizontalAlignment.Center;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(3, 0, 70);
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1445, 1055);
+            ClientSize = new Size(1264, 985);
+            Controls.Add(mathMenu);
+            Controls.Add(debugLabel);
             Controls.Add(pointsDisplay);
             Controls.Add(label1);
             DoubleBuffered = true;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             KeyDown += Keydown;
             KeyUp += Keyup;
+            mathMenu.ResumeLayout(false);
+            mathMenu.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -82,5 +188,13 @@ namespace Galaktyka_Matematyki
         #endregion
         private Label label1;
         private Label pointsDisplay;
+        private Label debugLabel;
+        private Panel mathMenu;
+        private TextBox redCount_box;
+        private Button sendButton;
+        private Label signLabel;
+        private TextBox greenCount_box;
+        private TextBox result_box;
+        private Label equalSign_label;
     }
 }
--- Galaktyka Matematyki/Form1.cs
+++ Galaktyka Matematyki/Form1.cs
@@ -1,3 +1,5 @@
+using Galaktyka_Matematyki.Properties;
+using Microsoft.Win32;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
 using System.Windows.Forms;
@@ -16,6 +18,11 @@ namespace Galaktyka_Matematyki
         private System.Windows.Forms.Timer generateTimer = new System.Windows.Forms.Timer();
 
         int points = 5;
+        int redCount = 0, greenCount = 0, result = 0;
+        int redStart = 0, greenStart = 0;
+
+        int signRand = 0;
+        string sign = "";
 
         public Form1()
         {
@@ -34,16 +41,16 @@ namespace Galaktyka_Matematyki
             generateTimer.Start();
 
             player.Name = "player";
-            player.Location = new Point(569, 800);
+            player.Location = new Point(565, 785);
             Controls.Add(player);
 
             pointsDisplay.Text = points.ToString();
+
+            CountRandomizer();
         }
 
         private void Generator(object sender, EventArgs e)
         {
-            Type obstacletype;
-
             int posX, posY;
             int type;
 
@@ -53,20 +60,43 @@ namespace Galaktyka_Matematyki
             type = new Random().Next(0, 1000);
             if (type >= 0 && type <= 100)
             {
-                obstacletype = typeof(BlackHole);
+                BlackHole blackHole = new BlackHole();
+                blackHole.Location = new Point(posX, posY);
+                gameobjects.Add(blackHole);
+                Controls.Add(blackHole);
+                blackHole.BringToFront();
             }
             else
             {
-                obstacletype = typeof(Asteroid);
+                Asteroid asteroid = new Asteroid();
+                asteroid.Location = new Point(posX, posY);
+                if (!(redCount <= 0 && greenCount <= 0))
+                {
+                    int color = new Random().Next(0, 3);
+                    switch (color)
+                    {
+                        case 0:
+                            if (redCount <= 0) break;
+                            asteroid.Image = Resources.asteroid_red;
+                            redCount--;
+                            break;
+                        case 1:
+                            if (greenCount <= 0) break;
+                            asteroid.Image = Resources.asteroid_green;
+                            greenCount--;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Maths();
+                }
+                gameobjects.Add(asteroid);
+                Controls.Add(asteroid);
+                asteroid.BringToFront();
             }
-
-            object obstacle = Activator.CreateInstance(obstacletype);
-
-            obstacle.Location = new Point(posX, posY);
-
-            gameobjects.Add(obstacle);
-            Controls.Add(obstacle);
-            obstacle.BringToFront();
 
             try
             {
@@ -94,6 +124,8 @@ namespace Galaktyka_Matematyki
                 Point p = new Point(player.Location.X + 5, player.Location.Y);
                 player.Location = p;
             }
+
+            debugLabel.Text = redStart + sign + greenStart + " = " + result;
         }
 
         private void Update2(object sender, EventArgs e)
@@ -115,6 +147,15 @@ namespace Galaktyka_Matematyki
             {
                 isRight = true;
             }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                pauseMenu();
+            }
+        }
+
+        private void pauseMenu()
+        {
+            throw new NotImplementedException();
         }
 
         private void Keyup(object sender, KeyEventArgs e)
@@ -142,9 +183,9 @@ namespace Galaktyka_Matematyki
                         Controls.Remove(go);
                         gameobjects.Remove(go);
                     }
-                    else
+                    else if (player.Bounds.IntersectsWith(go.Bounds) && go is BlackHole)
                     {
-                        points-=100;
+                        points -= 100;
                         pointsDisplay.Text = points.ToString();
                         Controls.Remove(go);
                         gameobjects.Remove(go);
@@ -152,6 +193,82 @@ namespace Galaktyka_Matematyki
                 }
             }
             catch { }
+        }
+
+        private void Maths()
+        {
+            mathMenu.Visible = true;
+            mathMenu.Enabled = true;
+            mathMenu.BringToFront();
+
+            updateTimer.Stop();
+            updateTimer2.Stop();
+            generateTimer.Stop();
+        }
+
+        private void CountRandomizer()
+        {
+            redCount = new Random().Next(1, 10);
+            greenCount = new Random().Next(1, 10);
+
+            redStart = redCount;
+            greenStart = greenCount;
+
+            signRand = new Random().Next(0, 3);
+
+            switch (signRand)
+            {
+                case 0:
+                    sign = "x";
+                    result = redCount * greenCount;
+                    break;
+                case 1:
+                    sign = "+";
+                    result = redCount + greenCount;
+                    break;
+                case 2:
+                    sign = "-";
+                    result = redCount - greenCount;
+                    break;
+                default:
+                    break;
+            }
+            signLabel.Text = sign;
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if (String.Equals(redCount_box.Text, redStart.ToString()) &&
+                String.Equals(greenCount_box.Text, greenStart.ToString()) &&
+                String.Equals(result_box.Text, result.ToString())
+               )
+            {
+                points += 100;
+                pointsDisplay.Text = points.ToString();
+            }
+
+            //    Nie dziala :(
+            try
+            {
+                foreach (GameObject go in gameobjects)
+                {
+                    Controls.Remove(go);
+                    gameobjects.Remove(go);
+                }
+            }
+            catch { }
+
+            mathMenu.Visible = false;
+            mathMenu.Enabled = false;
+            redCount_box.Text = "";
+            greenCount_box.Text = "";
+            result_box.Text = "";
+
+            CountRandomizer();
+
+            updateTimer.Start();
+            updateTimer2.Start();
+            generateTimer.Start();
         }
     }
 }
